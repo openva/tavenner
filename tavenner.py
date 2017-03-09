1aadd57c381ef9ed65b4597ceb2b502ed9c3c7d6
Waldo Jaquith <waldo@jaquith.org>
2017-03-08 19:28:13 -0500

    Initial scraper
    Toward #1.

---
+++ tavenner.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import errno
+import zipfile
+import sys
+import hashlib
+
+def main():
+    
+    from urllib2 import Request, urlopen, HTTPError
+    url_base = 'http://ethicssearch.dls.virginia.gov/ViewFormBinary.aspx?filingid='
+
+    errors = 0
+    i = 1
+    while True:
+
+        req = Request(url_base + str(i))
+
+        try:
+            f = urlopen(req)
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+            errors = 0
+            
+            local_file = open(str(i) + '.html', 'w')
+            local_file.write(f.read())
+            local_file.close()
+
+        except HTTPError as e:
+            sys.stdout.write(' ')
+            sys.stdout.flush()
+
+            errors += 1
+
+        i += 1
+
+        if errors >= 100:
+            break
+
+if __name__ == "__main__":
+    main()
